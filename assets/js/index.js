e6e61834b503e7c9f55a732c2a985bffc6ba1a71
AndresB1013 <giovanny_ballen@hotmail.com>
2023-04-03 00:18:36 -0500

    Agregar datos a la gráfica y a la tabla de datos

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,31 +2,7 @@
 (()=>{
     const baseUrl = 'https://www.datos.gov.co/resource/gt2j-8ykr.json';
     const myChart = document.getElementById('myChart').getContext('2d');
-
-    const grafica = new Chart(myChart, {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'darkblue'
-                ],
-                borderColor: [
-                    'darkblue'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
+    const tbCasos = document.getElementById('tbCasos');
 
     const loadData = (()=>{
             fetch(baseUrl,
@@ -34,9 +10,68 @@
                 method: "GET"
             })
                 .then(response => response.json())
-                .then(data => console.log(data))
+                .then(data => {
+                    console.log(data)
+
+                    const totalPersonasPorDepartamento = data.reduce((acumulador,item) =>{
+                        const departamento = item.departamento_nom;
+                        if (!acumulador[departamento]) {
+                            acumulador[departamento] = { departamento, total: 0 };
+                        }
+                        acumulador[departamento].total++;
+                        return acumulador;
+                    },{});
+
+                    const resultado ={
+                        status:"ok",
+                        data: Object.values(totalPersonasPorDepartamento)
+                    };
+                    console.log(resultado);
+
+                    let labels_for_chart = resultado.data.map((item)=>{
+                        return item.departamento;
+                    });
+
+                    let data_for_chart = resultado.data.map((item)=>{
+                        return item.total;
+                    });
+
+                    const grafica = new Chart(myChart, {
+                        type: 'bar',
+                        data: {
+                            labels: labels_for_chart,
+                            datasets: [{
+                                label: '# Total de Casos',
+                                data: data_for_chart,
+                                backgroundColor: '#00FF9E',
+                                borderColor: '#00FF9E',
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                                }
+                            }
+                        }
+                    });
+
+                    //Agregando datos a la tabla
+                    tbCasos.innerHTML = "";
+                    let id = 1;
+                    for (const item of resultado.data) {
+                        let tr =`
+                        <tr>
+                            <td>${id}</td>
+                            <td>${item.departamento}</td>
+                            <td>${item.total}</td>
+                        </tr>`;
+                        tbCasos.innerHTML += tr;
+                        id = id + 1;
+                    }
+                })
                 .catch(error => console.log(error))
         })
 
-        loadData()
+    loadData()
 })()
